519e5112ba6d8531abfce6688ca8f0d90aeaba8f
f1a3h <ihlwer@outlook.com>
2024-01-24 19:22:42 +0800

    finish lab01

--- labs/lab01/ex10_ll_cycle.c
+++ labs/lab01/ex10_ll_cycle.c
@@ -1,6 +1,34 @@
-#include <stddef.h>
 #include "ex10_ll_cycle.h"
+#include <stddef.h>
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+  /* TODO: Implement ll_has_cycle */
+  if (head == NULL) {
+    return 0;
+  }
+
+  node *fast_ptr = head->next;
+  if (fast_ptr != NULL) {
+    fast_ptr = fast_ptr->next;
+  } else {
+    return 0;
+  }
+  node *slow_ptr = head->next;
+
+  while (fast_ptr != slow_ptr) {
+    if (fast_ptr != NULL) {
+      fast_ptr = fast_ptr->next;
+
+      if (fast_ptr != NULL) {
+        fast_ptr = fast_ptr->next;
+      } else {
+        return 0;
+      }
+    } else {
+      return 0;
+    }
+
+    slow_ptr = slow_ptr->next;
+  }
+  return 1;
 }
