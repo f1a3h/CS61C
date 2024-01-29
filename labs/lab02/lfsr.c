6d61c3e4cd9cdc8a76033e542833f1aecab04423
f1a3h <ihlwer@outlook.com>
2024-01-29 16:46:05 +0800

    finish lab02

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -6,5 +6,9 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t lfsr = *reg;
+    uint16_t bit = (lfsr ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1u;
+    lfsr = (lfsr >> 1) | (bit << 15);
+    *reg = lfsr;
 }
 
