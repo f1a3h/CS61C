ef3cf5ebccd4846dcb2e8f9a44c715e375fc0d33
f1a3h <ihlwer@outlook.com>
2024-02-03 19:41:59 +0800

    finish proj2: part A

--- projs/proj2/src/argmax.s
+++ projs/proj2/src/argmax.s
@@ -15,20 +15,38 @@
 #   this function terminates the program with error code 77.
 # =================================================================
 argmax:
-
     # Prologue
+    addi t0, zero, 0 # max index
+    addi t1, zero, 0 # current index
 
+    blt zero, a1, loop_start
+
+    li a1, 77
+    j exit2
+
+    addi a1, a1, -1
 
 loop_start:
+    beq t1, a1, loop_end
 
+    slli t2, t1, 2
+    add t2, a0, t2
+    lw t2, 0(t2) # current element
+
+    slli t3, t0, 2
+    add t3, a0, t3
+    lw t3, 0(t3) # max element
+
+    blt t2, t3, loop_continue
+
+    mv t0, t1
 
 loop_continue:
-
+    addi t1, t1, 1
+    j loop_start
 
 loop_end:
-    
-
     # Epilogue
-
+    mv a0, t0
 
     ret
--- projs/proj2/src/relu.s
+++ projs/proj2/src/relu.s
@@ -15,24 +15,30 @@
 relu:
     # Prologue
 
+	add t0, zero, a0
+	addi t1, zero, 0
+
+	blt zero, a1, loop_start
+
+    li a1, 78
+    j exit2
 
 loop_start:
-    
+    beq t1, a1, loop_end
 
+    lw t2, 0(t0)
+    bge t2, zero, loop_continue
 
-
-
-
-
+    addi t2, zero, 0
 
 loop_continue:
+	sw t2, 0(t0)
 
-
+	addi t0, t0, 4
+    addi t1, t1, 1
+	j loop_start
 
 loop_end:
-
-
     # Epilogue
 
-    
 	ret
